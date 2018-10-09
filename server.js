5c7a2c184e7bd4768acd44a669c8812781f5b2b6
dimitrie13 <dimitrie13@outlook.com>
2018-10-08 20:51:02 -0400

    add project page

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'My Projects'
+  });
+});
+
+
 app.get('/bad', (req, res) => {
   res.send({
       errorMessage: 'Unable to fulfil this request'
